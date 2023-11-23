0d6b09be3cd70b7515eebabf2e4dbee7e0b43e1f
Rafael Horochovec <rl.horochovec@gmail.com>
2023-11-23 14:13:02 -0300

    Report

--- src/test/java/br/rafaelhorochovec/selenium/runner/CucumberRunnerTests.java
+++ src/test/java/br/rafaelhorochovec/selenium/runner/CucumberRunnerTests.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(tags = "",
         features = {"src/test/resources/features/login.feature"},
         glue = {"br.rafaelhorochovec.selenium.steps"},
-        plugin = {})
+        plugin= {"pretty", "html:test-output","json:target/cucumber/cucumber.json", "html:target/cucumber-html-report"})
 
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
 
